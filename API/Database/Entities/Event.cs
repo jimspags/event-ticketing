d2274b4c6b0dea21a34150d48c832ad69f176a4f
Jims <jamespaul.jp1@gmail.com>
2024-05-17 23:23:34 +0800

    Add context and event entity

---
+++ API/Database/Entities/Event.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Database.Entities
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        [MinLength(3), MaxLength(100)]
+        public string Title { get; set; }
+        [MinLength(3), MaxLength(200)]
+        public string Description { get; set; }
+        public double Price { get; set; }
+        [MinLength(3), MaxLength(200)]
+        public string Location { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        [MinLength(3), MaxLength(100)]
+        public string CreatedBy { get; set; }
+        public DateTimeOffset? ModifiedAt { get; set; }
+        [MinLength(3), MaxLength(100)]
+        public string? ModifiedBy { get; set; }
+    }
+}
