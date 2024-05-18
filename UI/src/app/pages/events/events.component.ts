42d12cf3c04808ea8a9fd46907072172d3ea9d56
Jims <jamespaul.jp1@gmail.com>
2024-05-18 20:11:40 +0800

    Integrate UI to API

--- UI/src/app/pages/events/events.component.ts
+++ UI/src/app/pages/events/events.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../services/event-service/event.service';
+import { EventModel } from '../../models/event-model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
+  /**
+   *
+   */
+  constructor(private eventService: EventService) {
+  }
 
+  events: EventModel[] = [];
+
+  ngOnInit(): void {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.eventService.getEvents()
+    .subscribe((result: EventModel[]) => {
+      this.events = result;
+      console.log(this.events)
+    })
+  }
 }
